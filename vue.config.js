9321d3288902ff4f54848784938066b26206d6f3
sylvia-H <sylvie11gaia@gmail.com>
2022-04-05 21:50:05 +0800

    revise root link

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Hex_Vue_Final/'
+    ? '/Hex_Vue_week7/'
     : '/',
   chainWebpack: (config) => {
     config.performance.set('hints', false);
